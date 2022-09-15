d6e1cd9b7e4b46d317759851c9e54f3945ced97d
SAMI <cformation75@gmail.com>
2022-09-15 19:36:21 +0200

    a f

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyconfig){
+          eleventyconfig.addPassthroughCopy('./src/style.css');
+          eleventyconfig.addPassthroughCopy('./src/assets');
+          return {
+      dir: {
+        input: "src",
+        output: "public",
+      },
+    };
+};
